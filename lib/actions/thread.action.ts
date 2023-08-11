05b7fb2c4f419bc74c921f92a92e6eeeaf7bbd34
shaoxuan <shaoxuanhinhua@gmail.com>
2023-08-11 23:59:00 +0800

    add fetchPosts and fetchThreadById function, as well as build up Thread Card Structure

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -34,3 +34,79 @@ export async function createThread({text,author,communityId,path} : Params) {
         throw new Error(`Error creating thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20){
+    // try {
+        connectToDB()
+
+        // Calculate the number of posts to skip
+        const skipAmount = (pageNumber - 1) * pageSize
+
+        // Fetch the posts that have no parents (top level threads ...)
+        const postsQuery = Thread.find({parentId : { $in: [null, undefined]}})
+        .sort({ createdAt: "desc"})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({path: "author", model: User})
+        .populate({
+            path: "children",
+            populate: {
+                path:"author",
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+        const totalPostsCount = await Thread.countDocuments({ parentId: {$in:[null, undefined]}})
+    
+        const posts = await postsQuery.exec()
+
+        // console.log("--------------posts---------------", posts ,typeof posts)
+
+        const isNext = totalPostsCount > skipAmount + posts.length
+
+        return { posts, isNext }
+
+    // } catch (error) {
+        
+    // }
+
+}
+
+export async function fetchThreadById(id: string) {
+    connectToDB()
+
+    try {
+
+        // TODO: Populate Community
+        const thread = await Thread.findById(id)
+            .populate({
+                path: "author",
+                model: User,
+                select: "_id id name image"
+            })
+            .populate({
+                path: "children",
+                populate: [
+                    {
+                        path: "author",
+                        model: User,
+                        select: "_id id name parentId image"
+                    },
+                    {
+                        path: "children",
+                        model: Thread,
+                        populate: {
+                            path: "author",
+                            model: User,
+                            select: "_id id name parentId image"
+                        }
+                    }
+                ]
+            }).exec()
+
+            return thread
+    } catch (error: any) {
+        throw new Error(`Error fetching thread: ${error.message}`)
+    }
+}
